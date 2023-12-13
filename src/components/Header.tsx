944499aa2e54f01d78004655e519c26a75498134
matsu <vikto132@gmail.com>
2023-12-13 07:09:21 +0700

    move wallet button to client

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { WalletButton } from './WalletButton';
 
 export const Header = () => {
-  const getAptosWallet = () => {
-    if (window.aptos) {
-      return window.aptos;
-    } else {
-      window.open('https://petra.app/', `_blank`);
-    }
-    return null;
-  };
-
-  const connectWallet = async () => {
-    const wallet = getAptosWallet();
-    if (!wallet) return;
-    try {
-      const response = await wallet.connect();
-      console.log(response); // { address: string, address: string }
-
-      const account = await wallet.account();
-      console.log(account); // { address: string, address: string }
-    } catch (error) {
-      // { code: 4001, message: "User rejected the request."}
-    }
-  };
-
   return (
     <div className="flex justify-between px-3">
       <div>
@@ -46,9 +24,7 @@ export const Header = () => {
               Use Cases
             </Link>
           </div>
-          <button className="btn-shadow text-primary rounded-full bg-white px-9 py-2 font-bold" onClick={connectWallet}>
-            Connect Wallet →
-          </button>
+          <WalletButton />
         </div>
       </div>
     </div>
---
+++ src/components/WalletButton.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+export const WalletButton = () => {
+  const getAptosWallet = () => {
+    if (window.aptos) {
+      return window.aptos;
+    } else {
+      window.open('https://petra.app/', `_blank`);
+    }
+    return null;
+  };
+
+  const connectWallet = async () => {
+    const wallet = getAptosWallet();
+    if (!wallet) return;
+    try {
+      const response = await wallet.connect();
+      console.log(response); // { address: string, address: string }
+
+      const account = await wallet.account();
+      console.log(account); // { address: string, address: string }
+    } catch (error) {
+      // { code: 4001, message: "User rejected the request."}
+    }
+  };
+  return (
+    <button className="btn-shadow text-primary rounded-full bg-white px-9 py-2 font-bold" onClick={() => connectWallet}>
+            Connect Wallet →
+          </button>
+  )
+}
